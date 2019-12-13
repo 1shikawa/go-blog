f25f52a03dc16c76a70e3077c01ddffd355bf88f
1shikawa <ishikawa.toru@gmail.com>
2019-12-13 18:54:15 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"log"
+
+	"go-blog/handler"
+	"go-blog/repository"
+
+	_ "github.com/go-sql-driver/mysql" // Using MySQL driver
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// const tmplPath = "C:\\Users\\toru-ishikawa\\go\\src\\go-blog\\src\\template\\"
+
+var db *sqlx.DB
+var e = createMux()
+
+func main() {
+	db = connectDB()
+	repository.SetDB(db)
+	// パスと処理関数(ハンドラ)の紐付け
+	/*
+		e.GET("/", articleIndex)
+		e.GET("/new", articleNew)
+		// コロンから始まる部分はパスパラメータ となり、任意の文字列を受理
+		e.GET("/:id", articleShow)
+		e.GET("/:id/edit", articleEdit)
+		// Webサーバーをポート番号 8080 で起動する
+
+	*/
+	e.GET("/", handler.ArticleIndex)
+	e.GET("/new", handler.ArticleNew)
+	e.GET("/:id", handler.ArticleShow)
+	e.GET("/:id/edit", handler.ArticleEdit)
+	e.POST("/", handler.ArticleCreate)
+	e.DELETE("/:id", handler.ArticleDelete)
+
+	e.Logger.Fatal(e.Start("localhost:1111"))
+}
+
+func createMux() *echo.Echo {
+	//インスタンス生成
+	e := echo.New()
+
+	//インスタンスに各種ミドルウェア設定
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CSRF())
+
+	e.Static("/css", "src/css")
+	e.Static("/js", "src/js")
+
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	return e
+}
+
+// ハンドラ関数定義
+// func articleIndex(c echo.Context) error {
+// 	/*
+// 	   HTTP リクエストの情報（リクエストの送信元や各種パラメータ等）は、
+// 	   echo.Context という構造体でハンドラ関数に渡される。
+// 	   リクエスト情報を構造体へ詰め替える作業や、ハンドラ関数に対して
+// 	   データを引数で受け渡す部分は、echoが担当。
+// 	*/
+// 	// return c.String(http.StatusOK, "Hello World")
+
+// 	data := map[string]interface{}{
+// 		// "Message":  "Hello World",
+// 		"Message":  "Article Index",
+// 		"Now time": time.Now(),
+// 	}
+// 	return render(c, "article\\index.html", data)
+// }
+
+// func articleNew(c echo.Context) error {
+// 	data := map[string]interface{}{
+// 		"Message": "Article New",
+// 		"Now":     time.Now,
+// 	}
+// 	return render(c, "article\\new.html", data)
+// }
+
+// func articleShow(c echo.Context) error {
+// 	// パスパラメータの :id の部分を抽出
+// 	// strconv.Atoiで文字列型→数値型へキャスト
+// 	id, _ := strconv.Atoi(c.Param("id"))
+
+// 	data := map[string]interface{}{
+// 		"Message": "Article Show",
+// 		"Now":     time.Now(),
+// 		"ID":      id,
+// 	}
+// 	return render(c, "article\\show.html", data)
+// }
+
+// func articleEdit(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
+
+// 	data := map[string]interface{}{
+// 		"Message": "Article Edit",
+// 		"Now":     time.Now(),
+// 		"ID":      id,
+// 	}
+// 	return render(c, "article\\edit.html", data)
+// }
+
+// func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+// 	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+// }
+
+// func render(c echo.Context, file string, data map[string]interface{}) error {
+// 	// 生成された HTML をバイトデータとして受け取る
+// 	b, err := htmlBlob(file, data)
+// 	if err != nil {
+// 		return c.NoContent(http.StatusInternalServerError)
+// 	}
+// 	// ステータスコード 200 で HTML データをレスポンス
+// 	return c.HTMLBlob(http.StatusOK, b)
+// }
+
+func connectDB() *sqlx.DB {
+	//環境変数から接続情報取得する場合もあり
+	// dsn := "workuser:Passw0rd!@tcp(127.0.0.1:3306)/techblog?parseTime=true&autocommit=0&sql_mode=%27TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY%27"
+	dsn := "root:mysitepass@tcp(127.0.0.1:3306)/techblog?parseTime=true&autocommit=0&sql_mode=%27TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY%27"
+
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	log.Println("db connection succeeded!!")
+	return db
+}
+
+// CustomValidator ...
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+// Validate ...
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
---
+++ repository/article_repository.go
@@ -0,0 +1,90 @@
+package repository
+
+import (
+	"database/sql"
+	"math"
+	"time"
+	"go-blog/model"
+)
+
+// func ArticleList() ([]*model.Article, error) {
+// 	// 実行する SQL
+// 	query := `SELECT * FROM articles;`
+
+// 	// データベースから取得した値を格納する変数を宣言
+// 	var articles []*model.Article
+
+// 	// Query を実行して、取得した値を変数に格納
+// 	if err := db.Select(&articles, query); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return articles, nil
+// }
+
+func ArticleListByCursor(cursor int)([]*model.Article,error){
+	if cursor <= 0{
+		cursor = math.MaxInt32
+	}
+	// ID の降順に記事データを 10 件取得するクエリ文字列を生成します。
+	query := `SELECT *
+	FROM articles
+	WHERE id < ?
+	ORDER BY id desc
+	LIMIT 10`
+
+	articles := make([]*model.Article, 0, 10)
+
+	if err := db.Select(&articles,query,cursor);err!=nil{
+		return nil,err
+	}
+	return articles,nil
+}
+
+
+func ArticleCreate(article *model.Article) (sql.Result, error) {
+  // 現在日時を取得します
+  now := time.Now()
+
+  // 構造体に現在日時を設定します。
+  article.Created = now
+  article.Updated = now
+
+  // クエリ文字列を生成します。
+  query := `INSERT INTO articles (title, body, created, updated)
+  VALUES (:title, :body, :created, :updated);`
+
+  // トランザクションを開始します。
+  tx := db.MustBegin()
+
+  // クエリ文字列と構造体を引数に渡して SQL を実行します。
+  // クエリ文字列内の「:title」「:body」「:created」「:updated」は構造体の値で置換されます。
+  // 構造体タグで指定してあるフィールドが対象となります。（`db:"title"` など）
+  res, err := tx.NamedExec(query, article)
+  if err != nil {
+    // エラーが発生した場合はロールバックします。
+    tx.Rollback()
+
+    // エラー内容を返却します。
+    return nil, err
+  }
+
+  // SQL の実行に成功した場合はコミットします。
+  tx.Commit()
+
+  // SQL の実行結果を返却します。
+  return res, nil
+}
+
+func ArticleDelete(id int)error{
+	query := `delete from articles where id =?;`
+
+	tx := db.MustBegin()
+	
+	if _,err := tx.Exec(query,id);err!=nil{
+		tx.Rollback()
+
+		return err
+	}
+	return tx.Commit()
+}
